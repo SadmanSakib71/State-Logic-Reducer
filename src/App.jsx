3c467c2d60161b3d3967798c0ef7df191997748f
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-02-05 07:36:32 +0600

    add task

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,30 @@ import { initialTasks } from "./data/Task";
 const App = () => {
   const [tasks, setTasks] = useState(initialTasks);
 
+  const getNextId = (data) => {
+    const maxId = data.reduce((prev, current) =>
+      prev && prev.id > current.id ? prev.id : current.id,
+    );
+    return maxId + 1;
+  };
+
+  const handleAddTask = (text) => {
+    setTasks([...tasks, { id: getNextId(tasks), text: text, done: false }]);
+  };
+
+  const handleChangeTask = (task) => {
+    const newTask = tasks.map((t) => (t.id === task.id ? task : t));
+    setTasks(newTask);
+  };
+
+  const handleDeleteTask = (taskId) => {
+    setTasks(tasks.filter((t) => t.id !== taskId));
+  };
+
   return (
     <>
       <h1>Sadman Sakib itinerary</h1>
-      <AddTask />
+      <AddTask onAdd={handleAddTask} />
       <TaskList tasks={tasks} />
     </>
   );
--- src/component/AddTask.jsx
+++ src/component/AddTask.jsx
@@ -1,8 +1,27 @@
-const AddTask = () => {
+import { useState } from "react";
+
+const AddTask = ({ onAdd }) => {
+  const [text, setText] = useState();
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <div>
-      <input placeholder="Add Task" />
-      <button>Add</button>
+      <input
+        placeholder="Add Task"
+        value={text}
+        onChange={(e) => handleTextChange(e)}
+      />
+      <button
+        onClick={() => {
+          setText("");
+          onAdd(text);
+        }}
+      >
+        Add
+      </button>
     </div>
   );
 };
